35b67f283fbc79824cd4edf84b85f50ca325306d
Jordan Deso <54602007+jrdeso@users.noreply.github.com>
2024-07-31 14:35:45 -0400

    initial process for collecting players.

---
+++ log_helper.py
@@ -0,0 +1,22 @@
+import logging
+from datetime import datetime, timedelta
+import time
+import sys
+
+class NFL_Logging:
+    def __init__(self):
+        self.logname = 'logs/nfl_logging.log'
+        logging.basicConfig(filename=self.logname, encoding='utf-8', level=logging.INFO, filemode='a')
+
+    def get_timestamp(self):
+        return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    
+    def label_log(self, script_name, function_name):
+        logging.info(f"{self.get_timestamp()}: Starting: {script_name} -> [{function_name}]")
+
+    def info(self, message):
+        logging.info(f"{self.get_timestamp()}: {message}")
+
+    def critical(self, message):
+        logging.critical(f"{self.get_timestamp()}: {message}")
+
---
+++ scrape.py
@@ -0,0 +1,58 @@
+import requests
+import pandas as pd
+import datetime
+from log_helper import NFL_Logging
+import os
+import inspect
+
+log = NFL_Logging()
+
+def get_players():
+    log.label_log(os.path.basename(__file__), inspect.currentframe().f_code.co_name)
+
+    players_url = "https://api.sleeper.app/v1/players/nfl"
+    
+    try:
+        response = requests.get(players_url)
+        response.raise_for_status()  # Will raise an HTTPError for bad responses
+        players_data = response.json()
+        players_df = pd.DataFrame.from_dict(players_data, orient='index')
+
+        # Filter for active players:
+        players_df = players_df[(players_df['status'] == 'Active')]
+
+        # Filter DF columns
+        filtered_columns = [
+            'player_id',
+            'first_name',
+            'last_name',
+            'team',
+            'age',
+            'position',
+            'depth_chart_order',
+            'injury_status',
+            'injury_body_part',
+            'injury_notes'
+        ]
+        players_df = players_df[filtered_columns]
+
+        # Filter for skill positions and filter out Inactive players
+        positions_to_include = ['QB', 'RB', 'TE', 'WR']
+        players_df = players_df[
+            (players_df['position'].isin(positions_to_include)) & 
+            (players_df['team'].notna())
+        ]
+
+        # For recording data to excel
+        excel_path = 'Excel Exports/nfl_players.xlsx'
+        players_df.to_excel(excel_path, index=False, engine='openpyxl')
+
+        return players_df
+        
+    except requests.exceptions.RequestException as e:
+        log.critical(f"Failed to retrieve data at {players_url}: {str(e)}")
+
+def main():
+    get_players()
+
+main()
