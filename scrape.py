abe7e3ead750e8691b39ebd196c1348605b6cba8
Jordan Deso <54602007+jrdeso@users.noreply.github.com>
2024-08-07 10:25:21 -0400

    Tidying up scrape class and edited initial db structure

--- scrape.py
+++ scrape.py
@@ -1,13 +1,10 @@
 import requests
 import pandas as pd
-import datetime
 from log_helper import NFL_Logging
 import os
 import inspect
 from dotenv import load_dotenv
-import json
 
-log = NFL_Logging()
 
 class Scrape:
 
@@ -15,7 +12,7 @@ class Scrape:
         """
         Initializes the Scrape class with API credentials and base URL.
         """
-        load_dotenv()
+        load_dotenv() ## Load .env file
         self.log = NFL_Logging()
         self.api_base_url = "https://tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com/"
         self.headers = {'x-rapidapi-key': os.getenv('API_KEY')}
@@ -88,6 +85,31 @@ class Scrape:
 
 
     def scrape_nfl_schedule(self, year):
+        """
+        Scrapes NFL schedule data for a given year from the NFL API and returns it as a pandas DataFrame.
+
+        This method sends a GET request to the NFL API to retrieve the schedule for all weeks of the specified year. 
+        It filters the data to include relevant game details and returns a DataFrame.
+
+        Args:
+            year (int): The year for which to retrieve the NFL schedule.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing information about NFL games, 
+                              including fields such as game ID, season, season type, 
+                              team IDs, team abbreviations, game date, and game time.
+
+        Raises:
+            requests.exceptions.RequestException: If the API request fails or there is an error in retrieving data.
+
+        Examples:
+            >>> scraper = Scrape()
+            >>> df = scraper.scrape_nfl_schedule(2023)
+            >>> print(df.head(1))
+            gameID season seasonType teamIDHome home teamIDAway away gameDate gameTime
+            20220804_JAX@LV 2023 Regular Season 1 JAX 2 LV 20220804 20:00
+        """
+        self.log.label_log(os.path.basename(__file__), inspect.currentframe().f_code.co_name)
         try:
             self.endpoint = "getNFLGamesForWeek"
             query = self.api_base_url + self.endpoint
@@ -124,6 +146,29 @@ class Scrape:
 
 
     def scrape_game_info(self, game):
+        """
+        Scrapes detailed game information for a specific game from the NFL API and returns it as a pandas DataFrame.
+
+        This method sends a GET request to the NFL API to retrieve detailed information about a specific game. 
+        It returns a DataFrame with the game details.
+
+        Args:
+            game (str): The game ID for which to retrieve the information.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing detailed information about the specified game.
+
+        Raises:
+            requests.exceptions.RequestException: If the API request fails or there is an error in retrieving data.
+
+        Examples:
+            >>> scraper = Scrape()
+            >>> df = scraper.scrape_game_info("20220804_JAX@LV")
+            >>> print(df.head(1))
+            gameID gameDate gameTime home away homeScore awayScore
+            20220804_JAX@LV 20220804 20:00 JAX LV 14 20
+        """
+        self.log.label_log(os.path.basename(__file__), inspect.currentframe().f_code.co_name)
         try:
             self.endpoint = "getNFLBoxScore"
             query = self.api_base_url + self.endpoint
@@ -144,90 +189,3 @@ class Scrape:
         except requests.exceptions.RequestException as e:
             self.log.critical(f"Failed to retrieve data at {query}: {str(e)}")
 
-
-    def clean_game_info_df(self, game_info_df):
-        # Open config.json file to reference defined dataframe structure
-        # (Put defined lists/dicts in config.json to declutter scrape.py)
-        with open('config.json') as f:
-            config = json.load(f)
-        game_filtered_fields = config['game_filtered_fields']
-        home_team_filtered_fields = config['home_team_filtered_fields']
-        home_team_renamed_fields = config['home_team_renamed_fields']
-        away_team_filtered_fields = config['away_team_filtered_fields']
-        away_team_renamed_fields = config['away_team_renamed_fields']
-        player_data_cols = config['player_data_cols']
-
-        # Extract general game info 
-        game_data_df = game_info_df[game_filtered_fields].copy()
-        # Extract home team data from game > Update names to clean up dataframe
-        home_team_data_df = game_info_df[home_team_filtered_fields].copy()
-        home_team_data_df.rename(columns=home_team_renamed_fields, inplace=True)
-        # Extract away team data from game > Update names to clean up dataframe
-        away_team_data_df = game_info_df[away_team_filtered_fields].copy()
-        away_team_data_df.rename(columns=away_team_renamed_fields, inplace=True)
-
-        # Extract home team and away team IDs, we assign these to the players game dataframe (helps keep track of who they were playing when they got these stats)
-        home_team_id = game_data_df.at[0,'teamIDHome']
-        away_team_id = game_data_df.at[0,'teamIDAway']
-
-        # Make dataframe to hold all player stats from game param:
-        players_stats_df = pd.DataFrame(columns=player_data_cols)
-
-        # Extract player stats from game_info_df param
-        for fieldname in game_info_df:
-            # Find a column with player stats in it
-            # > disregard any stats related to individual defensive player, kicking, or punting
-            if 'playerStats.' in fieldname and not any(substring in fieldname for substring in ['Defense', 'Punting', 'Kicking', 'scoringPlays']):
-                # Sample fieldnames that and how they're split:
-                # playerStats.3915508.longName          > [playerStats, 3915508, longName]
-                # playerStats.2578369.Receiving.targets > [playerStats, 2578369, Receiving, targets]
-                parts = fieldname.split('.')
-                player_id = parts[1] # playerID is always second index in list
-                
-                # Extract what stat category we're looking at (depends on size of list)
-                if len(parts) == 3:
-                    stat_category = parts[2]                  # e.g., longName
-                elif len(parts) == 4:
-                    stat_category = parts[2] + '.' + parts[3] # e.g., Receiving + . + targets
-                
-                # If player isn't in our dataframe yet, add them in
-                if player_id not in players_stats_df['playerID'].values:
-                    new_player = pd.DataFrame({'playerID': [player_id], stat_category: game_info_df[fieldname]})
-                    players_stats_df = pd.concat([players_stats_df, new_player], ignore_index=True)
-
-                # Identify stat_value, e.g., category is 'Receiving.targets', value is '8'
-                stat_value = game_info_df[fieldname].values[0]
-
-                # When we identify the team the player is on, insert the data on who they're playing against
-                if stat_category == 'teamID':
-                    if stat_value == home_team_id:
-                        players_stats_df.loc[players_stats_df['playerID'] == player_id, 'teamIDPlayedAgainst'] = away_team_id
-                        players_stats_df.loc[players_stats_df['playerID'] == player_id, 'homeOrAway'] = 'Home'
-                    elif stat_value == away_team_id:
-                        players_stats_df.loc[players_stats_df['playerID'] == player_id, 'teamIDPlayedAgainst'] = home_team_id
-                        players_stats_df.loc[players_stats_df['playerID'] == player_id, 'homeOrAway'] = 'Away'
-
-                # Insert stat into player's game data
-                players_stats_df.loc[players_stats_df['playerID'] == player_id, stat_category] = stat_value
-
-        # Filter out players with 0 stats (i.e., defensive players, kickers, punters, etc.)
-        # import list containing player stat columns
-        player_stat_columns = config['player_stat_columns']
-        # Create a boolean mask for rows where all specified columns are empty or NaN
-        mask = players_stats_df[player_stat_columns].isna().all(axis=1) | (players_stats_df[player_stat_columns] == '').all(axis=1)
-        # Drop the rows where the mask is True
-        players_stats_df = players_stats_df[~mask]
-
-
-        return game_data_df, home_team_data_df, away_team_data_df, players_stats_df
-
-
-
-
-
-def main():
-    scrape = Scrape()
-    df = scrape.scrape_players()
-    print(df.head())
-
-main()
